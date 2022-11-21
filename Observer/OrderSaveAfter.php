c9c94769257638859c97664454f5b4babc5b7bc4
jzambrano <jennifer.zambrano@2brains.cl>
2022-11-21 09:25:24 -0300

    validando los datos solo cuando sea blue

--- Observer/OrderSaveAfter.php
+++ Observer/OrderSaveAfter.php
@@ -70,7 +70,7 @@ class OrderSaveAfter implements ObserverInterface
         $state          = $observer->getEvent()->getOrder()->getState();
 	    $weight_uni 	= $this->_weigthStore;
 
-	 /**
+	    /**
          * I CONNECT TO THE SERVICE TO SEND THROUGH THE WEBHOOK
          */
         	$blueservice    = $this->_blueservice;
@@ -85,110 +85,111 @@ class OrderSaveAfter implements ObserverInterface
         $shippingAddress    = [];
         $billingAddress     = []; 
 
-        if(isset($shipping) && $shipping->getEntityId()){
-
-            $comuna         = $blueservice->eliminarAcentos($shipping->getCity());
-            $destricCity    = $blueservice->getGeolocation("{$comuna}");
-            $ShipiDistrict  = [];
-            $shipiRegion    = [];
-            
-            if(array_key_exists("districtCode",$destricCity)){
-                $ShipiDistrict = $destricCity['districtCode'];
-                $shipiRegion = $destricCity['regionCode'];
-            }
-
-            $billing            = $orders->getBillingAddress();
-            $comunaBilling      = $blueservice->eliminarAcentos($billing->getCity());
-            $destricBillingCity = $blueservice->getGeolocation("{$comunaBilling}");
-            $billiDistrict      = [];
-            $billiRegion        = [];
-
-            if(array_key_exists("districtCode",$destricCity)){
-                $billiDistrict = $destricBillingCity['districtCode'];
-                $billiRegion = $destricBillingCity['regionCode'];
-            }
-
-            $shippingAddress = [
-                "entity_id"         => $shipping->getEntityID(),
-                "parent_id"         => $shipping->getParentId(),
-                "quote_address_id"  => $shipping->getQuoteAddressId(),
-                "region_id"         => $shipping->getRegionId(),
-                "region"            => $shipping->getRegion(),
-                "postcode"          => $shipping->getPostCode(),
-                "lastname"          => $shipping->getLastname(),
-                "street"            => $shipping->getStreet(),
-                "city"              => $blueservice->eliminarAcentos($shipping->getCity()),
-                "district"          => $ShipiDistrict,
-                "state"             => $shipiRegion,
-                "email"             => $shipping->getEmail(),
-                "telephone"         => $shipping->getTelephone(),
-                "country_id"        => $shipping->getCountryId(),
-                "firstname"         => $shipping->getFirstname(),
-                "address_type"      => $shipping->getAddressType(),
-                "company"           => $shipping->getCompany()
-            ];
-
-            $billing = $orders->getBillingAddress();
-            $billingAddress = [
-                "entity_id"         => $billing->getEntityID(),
-                "parent_id"         => $billing->getParentId(),
-                "quote_address_id"  => $billing->getQuoteAddressId(),
-                "region_id"         => $billing->getRegionId(),
-                "region"            => $billing->getRegion(),
-                "postcode"          => $billing->getPostCode(),
-                "lastname"          => $billing->getLastname(),
-                "street"            => $billing->getStreet(),
-                "city"              => $blueservice->eliminarAcentos($billing->getCity()),
-		        "district"          => $billiDistrict,
-                "state"             => $billiRegion,
-                "email"             => $billing->getEmail(),
-                "telephone"         => $billing->getTelephone(),
-                "country_id"        => $billing->getCountryId(),
-                "firstname"         => $billing->getFirstname(),
-                "address_type"      => $billing->getAddressType(),
-                "company"           => $billing->getCompany()
-            ];
-        }
-
-	    $ProductDetail = array();
-	    $dimensiones = array();
-	    $objectManager  = \Magento\Framework\App\ObjectManager::getInstance();
-        $resource       = $objectManager->get(\Magento\Catalog\Model\ResourceModel\Product::class);
-
-	    foreach( $orders->getAllVisibleItems() as $item ) {
-
-	        $ProductDetail[] = $item->getData();
-            $alto            = $resource->getAttributeRawValue($resource->getIdBySku($item->getSku()), 'height', 0);
-            $ancho           = $resource->getAttributeRawValue($resource->getIdBySku($item->getSku()), 'width', 0);
-            $largo           = $resource->getAttributeRawValue($resource->getIdBySku($item->getSku()), 'large', 0);
-
-            if(empty($alto) || $alto == 0){
-                $alto  = 10;
-            }
-
-            if(empty($ancho) || $ancho == 0){
-                    $ancho  = 10;
-            }
-
-            if(empty($largo) || $largo == 0){
-                    $largo  = 10;
-            }
-
-            $dimensiones[]    = [
-                    "height"	=> $alto,
-                    "width"		=> $ancho,
-                    "large"		=> $largo
-            ];
-	    }
-
-        /**
-         * GETTING THE STORE URL BASE
-         */
-        $storeManager = $this->_storeManager;
-        $baseUrl = $storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB);
-
         if($state == 'processing' && ( $detailOrder['shipping_method'] =='bxexpress_bxexpress' || $detailOrder['shipping_method'] =='bxprioritario_bxprioritario' || $detailOrder['shipping_method'] == 'bxPremium_bxPremium' || $detailOrder['shipping_method'] == 'bxsameday_bxsameday')) {
-	        $typeCarrier = '';
+	        
+            if(isset($shipping) && $shipping->getEntityId()){
+
+                $comuna         = $blueservice->eliminarAcentos($shipping->getCity());
+                $destricCity    = $blueservice->getGeolocation("{$comuna}");
+                $ShipiDistrict  = [];
+                $shipiRegion    = [];
+                
+                if(array_key_exists("districtCode",$destricCity)){
+                    $ShipiDistrict = $destricCity['districtCode'];
+                    $shipiRegion = $destricCity['regionCode'];
+                }
+    
+                $billing            = $orders->getBillingAddress();
+                $comunaBilling      = $blueservice->eliminarAcentos($billing->getCity());
+                $destricBillingCity = $blueservice->getGeolocation("{$comunaBilling}");
+                $billiDistrict      = [];
+                $billiRegion        = [];
+    
+                if(array_key_exists("districtCode",$destricCity)){
+                    $billiDistrict = $destricBillingCity['districtCode'];
+                    $billiRegion = $destricBillingCity['regionCode'];
+                }
+    
+                $shippingAddress = [
+                    "entity_id"         => $shipping->getEntityID(),
+                    "parent_id"         => $shipping->getParentId(),
+                    "quote_address_id"  => $shipping->getQuoteAddressId(),
+                    "region_id"         => $shipping->getRegionId(),
+                    "region"            => $shipping->getRegion(),
+                    "postcode"          => $shipping->getPostCode(),
+                    "lastname"          => $shipping->getLastname(),
+                    "street"            => $shipping->getStreet(),
+                    "city"              => $blueservice->eliminarAcentos($shipping->getCity()),
+                    "district"          => $ShipiDistrict,
+                    "state"             => $shipiRegion,
+                    "email"             => $shipping->getEmail(),
+                    "telephone"         => $shipping->getTelephone(),
+                    "country_id"        => $shipping->getCountryId(),
+                    "firstname"         => $shipping->getFirstname(),
+                    "address_type"      => $shipping->getAddressType(),
+                    "company"           => $shipping->getCompany()
+                ];
+    
+                $billing = $orders->getBillingAddress();
+                $billingAddress = [
+                    "entity_id"         => $billing->getEntityID(),
+                    "parent_id"         => $billing->getParentId(),
+                    "quote_address_id"  => $billing->getQuoteAddressId(),
+                    "region_id"         => $billing->getRegionId(),
+                    "region"            => $billing->getRegion(),
+                    "postcode"          => $billing->getPostCode(),
+                    "lastname"          => $billing->getLastname(),
+                    "street"            => $billing->getStreet(),
+                    "city"              => $blueservice->eliminarAcentos($billing->getCity()),
+                    "district"          => $billiDistrict,
+                    "state"             => $billiRegion,
+                    "email"             => $billing->getEmail(),
+                    "telephone"         => $billing->getTelephone(),
+                    "country_id"        => $billing->getCountryId(),
+                    "firstname"         => $billing->getFirstname(),
+                    "address_type"      => $billing->getAddressType(),
+                    "company"           => $billing->getCompany()
+                ];
+            }
+    
+            $ProductDetail = array();
+            $dimensiones = array();
+            $objectManager  = \Magento\Framework\App\ObjectManager::getInstance();
+            $resource       = $objectManager->get(\Magento\Catalog\Model\ResourceModel\Product::class);
+    
+            foreach( $orders->getAllVisibleItems() as $item ) {
+    
+                $ProductDetail[] = $item->getData();
+                $alto            = $resource->getAttributeRawValue($resource->getIdBySku($item->getSku()), 'height', 0);
+                $ancho           = $resource->getAttributeRawValue($resource->getIdBySku($item->getSku()), 'width', 0);
+                $largo           = $resource->getAttributeRawValue($resource->getIdBySku($item->getSku()), 'large', 0);
+    
+                if(empty($alto) || $alto == 0){
+                    $alto  = 10;
+                }
+    
+                if(empty($ancho) || $ancho == 0){
+                        $ancho  = 10;
+                }
+    
+                if(empty($largo) || $largo == 0){
+                        $largo  = 10;
+                }
+    
+                $dimensiones[]    = [
+                        "height"	=> $alto,
+                        "width"		=> $ancho,
+                        "large"		=> $largo
+                ];
+            }
+    
+            /**
+             * GETTING THE STORE URL BASE
+             */
+            $storeManager = $this->_storeManager;
+            $baseUrl = $storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB);
+            $typeCarrier = '';
+            
             switch ($detailOrder['shipping_method']){
                 case 'bxexpress_bxexpress':
                     $typeCarrier = "EX";
